cd5c05ce57af2c2d9b33e2df7672a07b51c19cb4
Nils P. Ellingsen <q2i@storebrand.no>
2018-05-02 23:34:31 +0200

    uploading

---
+++ Details_files/Details.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20180501
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Details_files/DetailsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
